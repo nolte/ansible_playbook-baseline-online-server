767abc8a74136a5d99853cf1a5967f330a040d9b
nolte <nolte07@googlemail.com>
2022-01-29 17:42:47 +0100

    bump up tf version

--- terraform/configure/main.tf
+++ terraform/configure/main.tf
@@ -1,8 +1,8 @@
 # https://nicholasbering.ca/tools/2018/01/08/introducing-terraform-provider-ansible/
 module "ansible_local_dependencies" {
-  source    = "github.com/cloudposse/tf_ansible"
-  playbook  = "${path.module}/../../ansible/playbook-util-ansible-setup.yml"
-  dry_run   = false
+  source   = "github.com/cloudposse/tf_ansible"
+  playbook = "${path.module}/../../ansible/playbook-util-ansible-setup.yml"
+  dry_run  = false
 }
 
 module "ansible_provisioner" {
